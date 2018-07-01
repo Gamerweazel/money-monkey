967be9bc9e41b42ea5b0f52d1de34d3fc303d01a
Andrew Eames <gamerweazel@gmail.com>
2018-07-01 16:53:16 -0600

    Better looking router links.

--- client/components/MainComponent/template.js
+++ client/components/MainComponent/template.js
@@ -6,9 +6,15 @@ const template = `
           <img :class="{inverted: theme === 'dark'}" width="100" height="100" src="https://cdn4.iconfinder.com/data/icons/hana-emojis-monkey-edition-glyph/100/monkey_glyph-22-512.png" alt="avatar">
         </v-avatar>
       </v-toolbar-side-icon>
-      <router-link to="/dash">Home</router-link>
-      <router-link to="/stats">Stats</router-link>
-      <router-link to="/about">About Us</router-link>
+      <router-link to="/dash">
+        <v-btn flat small>Home</v-btn>
+      </router-link>
+      <router-link to="/stats">
+        <v-btn flat small>Stats</v-btn>
+      </router-link>
+      <router-link to="/about">
+        <v-btn flat small>About Us</v-btn>
+      </router-link>
       <v-spacer></v-spacer>
       <h4>Theme:</h4>
       <v-chip color="white" @click="setTheme('light')"></v-chip>
