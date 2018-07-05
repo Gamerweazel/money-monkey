887f68def74f8d554d420b26ea15224d364533ee
Andrew Eames <aeames@CIT-SCCT12-1206.cs.dixie.edu>
2018-07-05 12:48:31 -0600

    Getters and computed ranged table for stats.

--- client/components/StatsView/StatsView.js
+++ client/components/StatsView/StatsView.js
@@ -1,24 +1,78 @@
 const StatsView = Vue.component('stats-view', {
 	data: () => ({
 		startPicker: moment().subtract(1, 'month').format('YYYY-MM-DD'),
-		endPicker: moment().format('YYYY-MM-DD'),
+		endPicker: moment().add(1, 'day').format('YYYY-MM-DD'),
 	}),
 	computed: {
-		rangedExpenses() {
-			
+		expenses() {
+			const startUTC = moment(this.startPicker).unix()
+			const endUTC = moment(this.endPicker).unix()
+			return this.$store.getters.inRange(startUTC, endUTC)
+		},
+		items() {
+			return [
+				{ text: 'TOTAL', value: this.total },
+				{ text: 'Average', value: this.average },
+				{ text: 'Minimum', value: this.minimum },
+				{ text: 'Maximum', value: this.maximum },
+			]
+		},
+		total() {
+			return this.expenses.reduce((acc, curr) => acc + curr.amount, 0)
+		},
+		average() {
+			return this.total / this.expenses.length
+		},
+		minimum() {
+			return this.expenses.sort((a, b) => {
+				if (a.amount < b.amount) return -1
+				if (a.amount > b.amount) return 1
+				else return 0
+			})[0].amount
+		},
+		maximum() {
+			return this.expenses.sort((a, b) => {
+				if (a.amount > b.amount) return -1
+				if (a.amount < b.amount) return 1
+				else return 0
+			})[0].amount
 		}
 	},
 	template: `
 		<v-content>
 			<v-container>
-				<v-layout row justify-space-around>
-					<h1>Start Date</h1>
-					<h1>End Date</h1>
+
+				<v-layout row text-xs-center>
+
+					<v-container>
+						<h1>Start Date</h1>
+						<v-date-picker v-model="startPicker"></v-date-picker>
+					</v-container>
+
+					<v-container>
+						<h1>End Date</h1>
+						<v-date-picker v-model="endPicker"></v-date-picker>
+					</v-container>
+
 				</v-layout>
-				<v-layout row justify-space-around>
-					<v-date-picker v-model="startPicker"></v-date-picker>
-					<v-date-picker v-model="endPicker"></v-date-picker>
-				</v-layout>	
+
+				<v-layout row justify-center>
+					<h1>Statistics</h1>
+				</v-layout>
+
+				<v-layout row justify-center>
+					<v-flex xs8>
+						<div v-if="expenses.length">
+							<v-data-table :items=items class="elevation-1" hide-actions hide-headers>
+								<template slot="items" slot-scope="props">
+									<td class="text-xs-left"><b>{{ props.item.text }}</b></td>
+									<td class="text-xs-center">\$\{{ props.item.value.toFixed(2) }}</td>
+								</template>
+							</v-data-table>
+						</div>
+					</v-flex>
+				</v-layout>
+
 			</v-container>
 		</v-content>
 	`
